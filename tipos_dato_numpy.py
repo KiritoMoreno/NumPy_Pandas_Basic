44c188826fc0356910002eb7d48ceed1bb57e428
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-07-11 20:31:51 +0100

    Tipos de datos de numpy con Arrays

---
+++ tipos_dato_numpy.py
@@ -0,0 +1,29 @@
+import numpy as np 
+
+'''
+arr = np.array([1, 2, 3, 4])
+print(arr.dtype)
+
+arr = np.array([1, 2, 3, 4], dtype = 'float64')
+print(arr.dtype)
+
+# INT
+arr = np.array([1, 2, 3, 4])
+arr = arr.astype(np.float64)
+print(arr)
+
+# BOOLEAN
+arr = np.array([0, 1, 2, 3, 4])
+arr = arr.astype(np.bool_)
+print(arr)
+
+# String
+arr = np.array([0, 1, 2, 3, 4])
+arr = arr.astype(np.string_)
+print(arr)
+
+# De String a Número
+arr = np.array(['0', '1', '2', '3', '4'])
+arr = arr.astype(np.int8)
+print(arr)
+'''
