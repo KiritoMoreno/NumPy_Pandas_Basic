6001c3a6aaa22169ba9627c795e678d80928852f
kiritomoreno <moreno.algorithmictrading@gmail.com>
2023-07-13 19:11:27 +0100

    Hacemos la prueba de lo aprendido leyendo un archivo csv

--- read_csv y json.py
+++ read_csv y json.py
@@ -13,7 +13,13 @@ df_json_book =pd.read_json('hpcharactersdataraw.json')
 # df_json_book =pd.read_json('hpcharactersdataraw.json',typ = 'Series') -->Aqui llevamos una estructura JSON a una estructura pandas (typ='')
 # Simplemente me va a indexar una lista con los valores que tengo, pero en formato RAW. Esto es un panda series
 
-print(df_book)
+print(df_book.columns)
 print()
-print(df_json_book)
+print(df_json_book.columns)
+print()
 
+# leer el archivo csv world population
+df_world = pd.read_csv('world_population.csv', sep = ',', header = 0)
+df_world = df_world[df_world['Continent'] == 'South America']
+
+print(df_world)
